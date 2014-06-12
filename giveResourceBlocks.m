4134bfec9926fbaea7846f7b56ced9c23d5b0f36
bkoval <brt.kowal@gmail.com>
2014-06-12 20:29:29 +0200

    Give band to the users ;)

--- giveResourceBlocks.m
+++ giveResourceBlocks.m
@@ -1,6 +1,38 @@
-function [ updatedBand ] = giveResourceBlocks( band, activeUsers )
+function [ newBand ] = giveResourceBlocks( band, activeUsers )
 %GIVERESOURCEBLOCKS Gives resource blocks to the users
+nrOfUsers = size(activeUsers, 1)
+%We fairly give every user same amount of blocks
+blocksPerUser = floor( length(band) / nrOfUsers );
 
-
+%First the d2d users get their blocks next to each other
+for i=1:nrOfUsers
+    if(activeUsers(i, 3)) %==1 means user has d2d
+        userId = activeUsers(i);
+        blocksTaken = 0;
+        index = 0;
+        while(blocksTaken < blocksPerUser)
+            if(~band(index))
+                blocksTaken = blocksTaken + 1;
+                band(index) = userId;
+            end
+            index = index + 1;
+        end
+    end
 end
 
+%Next we fill the band for the non-d2d users
+for i=1:nrOfUsers
+    if(~activeUsers(i, 3)) %==1 means user has d2d
+        userId = activeUsers(i);
+        blocksTaken = 0;
+        index = 0;
+        while(blocksTaken < blocksPerUser)
+            if(~band(index))
+                blocksTaken = blocksTaken + 1;
+                band(index) = userId;
+            end
+            index = index + 1;
+        end
+    end
+end
+            
