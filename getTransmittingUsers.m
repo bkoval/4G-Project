0d59258f3c3f1383941bc374cdd5c6f40c75af07
bkoval <brt.kowal@gmail.com>
2014-06-14 21:27:23 +0200

    Fixing band and active users

--- getTransmittingUsers.m
+++ getTransmittingUsers.m
@@ -5,7 +5,7 @@ function [ activeUsers ] = getTransmittingUsers( users, numberOfActiveUsers )
 % returns [vector]
 
 %We can control the number of the active users in a cell
-activeUsers = zeros (numberOfActiveUsers, 1);
+activeUsers = zeros (numberOfActiveUsers, 4);
 
 %Max user id is equal to the number of lines in the users array
 maxId = size(users, 1);
@@ -17,6 +17,6 @@ for i=1:numberOfActiveUsers
         %Needs to be an equal-probability rand
         activeUser = floor(rand() * maxId);
     end
-    activeUsers(i) = activeUser;
+    activeUsers(i, :) = users(activeUser, :);
 end
 
--- giveResourceBlocks.m
+++ giveResourceBlocks.m
@@ -1,15 +1,17 @@
 function [ newBand ] = giveResourceBlocks( band, activeUsers )
 %GIVERESOURCEBLOCKS Gives resource blocks to the users
+%band is a vector of resource block ids
+%activeUsers is a matrix of activeUsers
 nrOfUsers = size(activeUsers, 1)
 %We fairly give every user same amount of blocks
 blocksPerUser = floor( length(band) / nrOfUsers );
 
 %First the d2d users get their blocks next to each other
 for i=1:nrOfUsers
-    if(activeUsers(i, 3)) %==1 means user has d2d
-        userId = activeUsers(i);
+    if(activeUsers(i, 4)) %==1 means user has d2d
+        userId = activeUsers(i, 1);
         blocksTaken = 0;
-        index = 0;
+        index = 1;
         while(blocksTaken < blocksPerUser)
             if(~band(index))
                 blocksTaken = blocksTaken + 1;
@@ -22,10 +24,10 @@ end
 
 %Next we fill the band for the non-d2d users
 for i=1:nrOfUsers
-    if(~activeUsers(i, 3)) %==1 means user has d2d
-        userId = activeUsers(i);
+    if(~activeUsers(i, 4)) %==1 means user has d2d
+        userId = activeUsers(i, 1);
         blocksTaken = 0;
-        index = 0;
+        index = 1;
         while(blocksTaken < blocksPerUser)
             if(~band(index))
                 blocksTaken = blocksTaken + 1;
@@ -35,4 +37,6 @@ for i=1:nrOfUsers
         end
     end
 end
+%return the newly updated band
+newBand = band;
             
