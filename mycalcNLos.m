1f118b9f406e44b56f06ba54794ed3cffdb9f152
fmajerowski <filip.majerowski@gmail.com>
2014-06-15 18:59:10 +0200

    Create mycalcNLos.m

---
+++ mycalcNLos.m
@@ -0,0 +1,34 @@
+function [ PL ] = mycalcNLos( d,ch )
+%ISLOS Summary of this function goes here
+%   Detailed explanation goes here
+
+%Dokument Rep. ITU-R M.2135-1 33 str. 31
+
+% Musimy policzyc NLos
+
+% Funckja przyjmuje jako argument odleglosc od stacji bazowej
+
+% Zwraca wartoÄšÂĂÂ NLos
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+%Wartosci stale
+hbs = 25; %wysokosc stacji bazowej
+fc = ch; %czestotliwoc srodkowa - tabela na stronie 14?
+c = 300000000; %predkosc swiatla
+hUT = 1.5; %wysokosc terminala
+h = 20; %srednia wysokosc budynkow
+W = 20; %szerokosc ulic
+
+
+%Najpierw liczymy dbp, ktore okresla wg jakiego wzoru liczymy LOS
+%dbp = 4*hbs*hUT*fc/c
+
+if (d>10 && d<5000)  %zaleznosc wzieta z ostatniej kolumny tabeli
+PL = 161.04-7.1*log10(W)+7.5*log10(h)-(24.37-3.7*(h/hbs)^2)*log10(hbs)+(43.42-3.1*log10(hbs))*(log10(d)-3)+20*log10(fc)-(3.2*(log10(11.75*hUT))^2-4.97);
+else
+PL=0       
+    end
+
+end
+
+
