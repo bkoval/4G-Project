85374fd1ac158e09303dcc6dc29786140d1ea45c
bkoval <brt.kowal@gmail.com>
2014-06-16 21:27:50 +0200

    Add examples to user object

--- main.m
+++ main.m
@@ -1,6 +1,16 @@
-users = mycreateUsers();
+for i = 1: 100
+    %new user with id=i, x coord = 100, y  = 200, is d2d
+    users(i) = User(i, 100, 200, true);
+end
 
-users = giveResourceBlocks(2000, 2200, 20, users);
+%set user with id 2 as a d2d head to user with id 5
+users(2) = users(2).setD2DProperties(true, 5);
 
-%calculate throughput for each user and sum
-[thr,sum] = calculateThroughput(activeUsers);
+%add a few resource blocks (ids) to the user with id 7
+users(7) = users(7).addBlock(10);
+users(7) = users(7).addBlock(11);
+users(7) = users(7).addBlock(12);
+
+%Print to see what up
+users(2)
+users(7)
