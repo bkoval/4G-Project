845bb4f5a4c371d8dbf59eeb758bddd8e8817a97
bkoval <brt.kowal@gmail.com>
2014-06-16 22:45:49 +0200

    Cleanups & Fixes

--- getTransmittingUsers.m
+++ getTransmittingUsers.m
@@ -8,15 +8,15 @@ function [ activeUsers ] = getTransmittingUsers( users, numberOfActiveUsers )
 activeUsers = zeros (numberOfActiveUsers, 1);
 
 %Max user id is equal to the number of lines in the users array
-maxId = size(users, 1);
+maxId = length(users);
 
 for i=1:numberOfActiveUsers
     activeUser = 0;
     %Find unique, non-zero id's
-    while (activeUser == 0 || ismember(activeUser, activeUsers) || isRegular || isD2DHead)
+    while (activeUser == 0 || ismember(activeUser, activeUsers))
         %Needs to be an equal-probability rand
         activeUser = floor(rand() * maxId);
     end
-    activeUsers(i) = users(activeUser, 1);
+    activeUsers(i) = activeUser;
 end
 
